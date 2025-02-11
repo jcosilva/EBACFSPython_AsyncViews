4d5e8777b865b4408a016ea8cc8a5eb953b3089a
Julio Silva <silvajulio@hotmail.com>
2025-02-11 09:37:39 -0500

    Correção do Código: 20250211

--- asyncviews/views.py
+++ asyncviews/views.py
@@ -1,6 +1,5 @@
 import asyncio
 import httpx
-from time import sleep
 from django.http import HttpResponse
 
 async def http_call_async():
@@ -8,20 +7,9 @@ async def http_call_async():
         await asyncio.sleep(1)
         print(f'{num} - async')
     async with httpx.AsyncClient() as client:
-        r = await client.get('https://httpbin.org')
+        r = await client.get("https://httpbin.org/")
         print(r)
 
-def http_call_sync():
-    for num in range(1, 6):
-        sleep(1)
-        print(f'{num} - sync')
-    r = httpx.get('https://httpbin.org/')
-    print(r)
-
 async def async_view(request):
-    asyncio.create_task(http_call_async())
-    return HttpResponse('Non-blocking HTTP request')
-
-def sync_view(request):
-    http_call_sync()
-    return HttpResponse('Blocking HTTP request')
+    await asyncio.create_task(http_call_async())
+    return HttpResponse('Async Number Printing')
